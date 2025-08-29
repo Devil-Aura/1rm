34f9930f9bc430307be8fa73ca41150db1863c5d
Devil-Aura <imh203356@gmail.com>
2025-08-29 17:13:36 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+API_ID = int(os.getenv("API_ID", "YOUR_API_ID_HERE"))
+API_HASH = os.getenv("API_HASH", "YOUR_API_HASH_HERE")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
